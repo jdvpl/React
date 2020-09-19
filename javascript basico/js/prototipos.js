2998394b53aad4f55f0da6fc94b3b5e41ba9387d
jdvpl <juanda554242@gmail.com>
2020-09-19 01:15:36 -0500

    destructurin objects

--- javascript basico/js/prototipos.js
+++ javascript basico/js/prototipos.js
@@ -1,1 +1,25 @@
-// funcion
+// extrayendo valores de los objetos
+const aprendiendoJS={
+    version:{
+        nueva:'ES6+',
+        anterior:'ES5'
+    },
+    frameworks:['React','VueJS','AngularJS']
+}
+console.log(aprendiendoJS);
+
+// version anterior para extraer los valores
+
+let version1=aprendiendoJS.version.nueva;
+let frame=aprendiendoJS.frameworks[2];
+console.log(frame);
+console.log(version1);
+
+// destructuring forma nueva
+
+let {version,frameworks}=aprendiendoJS;
+console.log(frameworks)
+console.log(version);
+
+let {anterior}=aprendiendoJS.version;
+console.log(` la version anterior es: ${anterior}`)
