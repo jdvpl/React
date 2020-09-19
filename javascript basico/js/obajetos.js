decbbd910f1d12b4002c947d1e5b2dbc359847ed
jdvpl <juanda554242@gmail.com>
2020-09-19 00:35:48 -0500

    Object Constructor

--- javascript basico/js/obajetos.js
+++ javascript basico/js/obajetos.js
@@ -15,3 +15,18 @@ console.log(Datos.email);
 console.log(Datos['profesion']);
 
 // object constructor
+
+
+function Tarea(nombre, urgencia){
+    this.nombre=nombre;
+    this.urgencia=urgencia;
+}
+const tarea1=new Tarea("Aprendiendo Javascript y React ", "Urgente");
+const tarea2=new Tarea("Aprendiendo Python en la Nacional", "Urgente");
+const tarea3=new Tarea("Aprendiendo Goland y React ", "Urgente");
+const tarea4=new Tarea("Aprendiendo Seguridad informatica ", "Urgente");
+
+console.log(tarea1);
+console.log(tarea2);
+console.log(tarea3);
+console.log(tarea4);
