e4a01085b3423cf828703fdabee4aee2e3e8c21b
Juan Daniel <juanda554242@gmail.com>
2020-09-21 13:41:24 -0500

    object keys e object destructuring

--- javascript basico/js/7arreglos.js
+++ javascript basico/js/7arreglos.js
@@ -6,9 +6,28 @@ const carrito = ['priducto 1', 'producto 2', 'producto 3'];
 const AppContenedor = document.querySelector('#carrito');
 // AppContenedor.innerHTML = carrito; //con este metodo se agregan todos al mismo tiempo
 // de manera individual
-let htmlcarrito = ''; //se inicia de manera vacia para ir agregando
-carrito.forEach(producto => {
-    htmlcarrito += `<li>${producto}</li>`;
+// let htmlcarrito = ''; //se inicia de manera vacia para ir agregando
+// carrito.forEach(producto => {
+//     htmlcarrito += `<li>${producto}</li>`;
 
-})
-AppContenedor.innerHTML = htmlcarrito;
+// })
+// AppContenedor.innerHTML = htmlcarrito; //para imprimir en el DOM
+
+// con .map
+carrito.map(productos => {
+    return `el prodcuto es ${productos}`;
+});
+
+// object keys
+const persona = {
+        nombre: 'Juan',
+        profesion: 'software engeneer',
+        edad: 25
+    }
+    // destructuring accedes a la clave del objeto igualandolo con el objeto
+    // asi lo imprimes
+
+const { nombre } = persona;
+console.log(nombre)
+    // con object keys es menos codigo e imprimes todos los valores
+console.log(Object.keys(persona))
