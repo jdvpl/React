f8f149c06ca79147b79c650bea8c800b994fe39b
Juan Daniel <juanda554242@gmail.com>
2020-09-21 13:15:52 -0500

    object literal enhacenment

---
+++ javascript basico/js/objecliteral.js
@@ -0,0 +1,16 @@
+// objecto literal enhancemnet
+const banda = 'metalica';
+const genero = 'Heavy metal';
+const canciones = ["Enter Sandman", "Master of pupets", "All night mare long"];
+
+// const metalica = {
+//     banda: banda,
+//     genero: genero,
+//     canciones: canciones
+// }
+// console.log(metalica)
+
+
+// forma nueva
+const metalica = { banda, genero, canciones };
+console.log(metalica);
