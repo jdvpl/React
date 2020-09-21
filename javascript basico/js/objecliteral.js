9cba25b35dd37ce6c8cb19f5d7a790d81ee7a23d
Juan Daniel <juanda554242@gmail.com>
2020-09-21 13:21:33 -0500

    pasando funciones en los objetos

--- javascript basico/js/objecliteral.js
+++ javascript basico/js/objecliteral.js
@@ -1,16 +1,28 @@
-// objecto literal enhancemnet
-const banda = 'metalica';
-const genero = 'Heavy metal';
-const canciones = ["Enter Sandman", "Master of pupets", "All night mare long"];
+// // objecto literal enhancemnet
+// const banda = 'metalica';
+// const genero = 'Heavy metal';
+// const canciones = ["Enter Sandman", "Master of pupets", "All night mare long"];
 
-// const metalica = {
-//     banda: banda,
-//     genero: genero,
-//     canciones: canciones
-// }
-// console.log(metalica)
+// // const metalica = {
+// //     banda: banda,
+// //     genero: genero,
+// //     canciones: canciones
+// // }
+// // console.log(metalica)
 
 
-// forma nueva
-const metalica = { banda, genero, canciones };
-console.log(metalica);
+// // forma nueva
+// const metalica = { banda, genero, canciones };
+// console.log(metalica);
+
+// agregando funciones en un objeto
+const person = {
+    nombre: "Juan",
+    trabajo: "Full Stack Developer",
+    edad: 25,
+    musicaRock: true,
+    mostrarInformacion() {
+        console.log(`${this.nombre} es ${this.trabajo} su edad es ${this.edad}`)
+    }
+}
+person.mostrarInformacion();
