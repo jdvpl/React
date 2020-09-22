932f732c22282786217a1ea0a3b97678538ac327
Juan Daniel <juanda554242@gmail.com>
2020-09-22 13:22:28 -0500

    clases heredando del padre al hijo

--- javascript basico/js/12clases.js
+++ javascript basico/js/12clases.js
@@ -3,15 +3,33 @@
 //     this.nombre = tarea
 // )
 // clases hoy en dia
+// creando clases heredando
 
-class Tarea {
+class Tarea { //padre clase
     constructor(nombre, prioridad) {
             this.nombre = nombre;
             this.prioridad = prioridad;
         }
         // creando metodo
     mostrar() {
-        return (`${this.nombre} tiene una priridad ${this.prioridad}`)
+        console.log(`${this.nombre} tiene una priridad ${this.prioridad}`)
+    }
+}
+class ComprasPendiente extends Tarea {
+    constructor(nombre, prioridad, cantidad, precio, total) {
+        super(nombre, prioridad); //con el super hereda las dos variables que estan en la clase principal
+        this.cantidad = cantidad;
+        this.precio = precio;
+        this.total = precio * cantidad;
+
+    }
+    mostrar() {
+        super.mostrar();
+        console.log(`voy a comprar ${this.cantidad}, el precio unitario es: ${this.precio}
+        valor a pagar: ${this.total}`)
+    }
+    hola() {
+        return 'insecto'
     }
 }
 // crear los objetos
@@ -21,7 +39,11 @@ let tarea2 = new Tarea('Aprender React', 'Alta');
 let tarea3 = new Tarea('Aprender Python', 'Alta');
 let tarea4 = new Tarea('Aprender R', 'Alta');
 
-console.log(tarea1.mostrar());
-console.log(tarea2.mostrar());
-console.log(tarea3.mostrar());
-console.log(tarea4.mostrar());
+// console.log(tarea1.mostrar());
+// console.log(tarea2.mostrar());
+// console.log(tarea3.mostrar());
+// console.log(tarea4.mostrar());
+
+let compra1 = new ComprasPendiente('Jabon', 'Alta', 2, 3000, 'valor a pagar')
+compra1.mostrar();
+console.log(compra1.hola())
