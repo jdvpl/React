64cbed6b34ab6656bdd747da9af8569a4b669955
Juan Daniel <juanda554242@gmail.com>
2020-10-04 22:59:10 -0500

    importando states, hooks >16 creando listado de productos

--- basicos/basico/src/App.js
+++ basicos/basico/src/App.js
@@ -1,8 +1,17 @@
-import React,{Fragment} from 'react';
+import React,{Fragment,useState} from 'react';
 import Header from './components/Header'; /** importar el componete header */
 import Footer from './components/Footer'; 
 
 function App() {
+    // listado de productos
+    const [productos, guardarPorudctos ]=useState([
+        {id:1,nombre:'MSI GE63 Raider 8RE', precio:5000000},
+        {id:2,nombre:'Mac Catalina', precio:10000000},
+        {id:3,nombre:'Asus Rog', precio:5000000},
+        {id:4,nombre:'Lenovo', precio:5000000},
+    ]);
+
+
     //obtener la fecha
     const fecha=new Date().getFullYear();
 
