15d9ec3c391886990b9ea2ebb12e85d41cfb0303
Juan Daniel <juanda554242@gmail.com>
2020-10-04 22:09:24 -0500

    en jsx es className no class para darle estilo

--- basicos/basico/src/components/Header.jsx
+++ basicos/basico/src/components/Header.jsx
@@ -2,21 +2,10 @@ import React from 'react';
 
 
 function Header(){
-    //algo de jsx
-    //este codigo solo debe ir antes del return
-    const edad=18;
-    let mensaje;
-
-    if (edad>=18){
-        mensaje='Eres mayor de edad';
-    }else{
-        mensaje='Eres menor de edad';
-    }
+  
 
     return( /** lo que se va a ver en el navegador */
-        <div>
-            <h2>Desde el Header {mensaje} jiren</h2>
-        </div>
+       <h1 className="encabezado">Tienda Virtual</h1>
        
 
 
