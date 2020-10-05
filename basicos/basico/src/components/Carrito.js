5af117ed26c14c5a24f00b47af49fb4a4bca193f
Juan Daniel <juanda554242@gmail.com>
2020-10-05 00:06:58 -0500

    Mostrando los elementos del carrito

--- basicos/basico/src/components/Carrito.js
+++ basicos/basico/src/components/Carrito.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import './carrito.css';
+import Producto from './Producto';
 
-const Carrito = () =>  (  
+
+const Carrito = ({carrito}) =>  (  
     <div className="carrito">
         <h3>Tu carrito de compras</h3>
+        {carrito.length===0
+        ?
+            <p>No hay elementos</p>
+        :
+        carrito.map(producto => (
+            <Producto
+                key={producto.id}
+                producto={producto}
+            />
+        ))}
+
     </div>
 );
 
