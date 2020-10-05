ae1ec5acef6441ab039aab7526e2a91487c36940
Juan Daniel <juanda554242@gmail.com>
2020-10-05 15:20:49 -0500

    aplicando estilos con skeleton

--- basicos/citas/src/App.js
+++ basicos/citas/src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
-
-
+import React, { Fragment } from "react";
 
 function App() {
   return (
-    <h1>Administrador de pacientes</h1>
+    <Fragment>
+      <h1>Administrador de pacientes</h1>
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">1</div>
+
+          <div className="one-half column">2</div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
