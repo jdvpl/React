3e6475a33d30d79992a941219748b67d1312047e
Juan Daniel <juanda554242@gmail.com>
2020-10-05 15:25:59 -0500

    creando el componente del Formulario

--- basicos/citas/src/App.js
+++ basicos/citas/src/App.js
@@ -1,15 +1,28 @@
-import React, { Fragment } from "react";
+import React,{Fragment} from 'react';
+import Formulario from './components/Formulario';
+
+
+
 
 function App() {
   return (
     <Fragment>
-      <h1>Administrador de pacientes</h1>
+    <h1 >Administrador de pacientes</h1>
       <div className="container">
-        <div className="row">
-          <div className="one-half column">1</div>
 
-          <div className="one-half column">2</div>
+      
+      <div className="row">
+        <div className="one-half column">
+
+          <Formulario/>
+          
         </div>
+
+          
+        <div className="one-half column">
+          2
+        </div>
+      </div>
       </div>
     </Fragment>
   );
