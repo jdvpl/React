2e21f3d0292003d117c5a387e9f9d332dbf6ce16
Juan Daniel <juanda554242@gmail.com>
2020-10-05 21:18:41 -0500

    Mostrando las Citas

--- basicos/citas/src/components/Cita.js
+++ basicos/citas/src/components/Cita.js
@@ -1,9 +1,14 @@
 import React,{Fragment} from 'react';
 
-const Cita = () => ( 
+const Cita = ({cita}) => ( 
     <Fragment>
         <div className="cita">
-            <p>Mascota: <span></span></p>
+            <p>Mascota: <span>{cita.mascota}</span></p>
+            <p>Edad: <span>{cita.edad} Meses</span></p>
+            <p>Dueño: <span>{cita.propietario}</span></p>
+            <p>Fecha: <span>{cita.fecha}</span></p>
+            <p>Hora: <span>{cita.hora}</span></p>
+            <p>Sintomas: <span>{cita.sintomas}</span></p>
         </div>
     </Fragment>
 );
