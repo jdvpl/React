dcd1eaae58c6b45f11ccab19ea066306f459e607
Juan Daniel <juanda554242@gmail.com>
2020-10-06 00:31:26 -0500

    terminando los ajustes de css de proyecto

--- basicos/presupuesto/src/App.js
+++ basicos/presupuesto/src/App.js
@@ -4,7 +4,9 @@ import React from 'react';
 function App() {
   return (
     <div className="container">
-      <h1>Lol</h1>
+      <header>
+        <h1>Gasto Semanal</h1>
+      </header>
     </div>
   );
 }
