fdee22ad0ec91824510388f91b6c8f3297b217f4
Juan Daniel <juanda554242@gmail.com>
2020-10-18 18:39:53 -0500

    integrando el spinner desde la pagina le das en source https://tobiasahlin.com/spinkit/

---
+++ basicos/cotizador/src/components/Spinner.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// pagina del spinner
+// ! https://tobiasahlin.com/spinkit/
+const Spinner = () => {
+    return ( 
+    <div className="sk-chase">
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+    </div>
+     );
+}
+ 
+export default Spinner;
