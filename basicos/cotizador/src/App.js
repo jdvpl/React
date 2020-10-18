c362a73dcbbe93839d0403d9129cca7f6a347daf
Juan Daniel <juanda554242@gmail.com>
2020-10-18 15:00:58 -0500

    creando el componente del form

--- basicos/cotizador/src/App.js
+++ basicos/cotizador/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import styled from '@emotion/styled';
+import Formulario from './components/Formulario';
 
 const Contenedor=styled.div`
   max-width:600px;
@@ -22,7 +23,7 @@ function App() {
       </Contenedor>
 
       <ContenedorFormulario>
-        
+        <Formulario/>
       </ContenedorFormulario>
      
     </div>
