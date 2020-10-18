40c2ef7cbdea94bcd15acb0cc446259f7aefcd14
Juan Daniel <juanda554242@gmail.com>
2020-10-18 14:31:14 -0500

    creando el componente para el titulo

--- basicos/cotizador/src/App.js
+++ basicos/cotizador/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Header from './components/Header';
+
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Cotizador de seguros</h1>
+      <Header
+        titulo="Cotizador de Seguros"
+      />
     </div>
   );
 }
