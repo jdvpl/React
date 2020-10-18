29bf263665b4c3a405943a26a1fdbc53832dc51f
Juan Daniel <juanda554242@gmail.com>
2020-10-18 14:48:56 -0500

    creando mas styled components

--- basicos/cotizador/src/App.js
+++ basicos/cotizador/src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import Header from './components/Header';
+import styled from '@emotion/styled';
 
+const Contenedor=styled.div`
+  max-width:600px;
+  margin:0 auto;
 
+`;
+const ContenedorFormulario=styled.div`
+  background-color:#fff;
+  padding:3rem;
+`;
 
 function App() {
   return (
     <div className="App">
+      <Contenedor>
       <Header
         titulo="Cotizador de Seguros"
       />
+      </Contenedor>
+
+      <ContenedorFormulario>
+        
+      </ContenedorFormulario>
+     
     </div>
   );
 }
