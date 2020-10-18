be7a1a6f316ec7062e0fbd32e928d9cda188ead8
Juan Daniel <juanda554242@gmail.com>
2020-10-18 14:44:43 -0500

    styled component ver anterior commit para instalar

--- basicos/cotizador/src/components/Header.js
+++ basicos/cotizador/src/components/Header.js
@@ -2,16 +2,22 @@ import React from 'react';
 import styled from '@emotion/styled';
 
 const ContenedotHeader=styled.header`
-    background-color:#d21000;
+    background-color:#26C6DA;
     padding:10px;
     font-weight:bold;
     color:#FFFFFF;
+    
 `;
-
+const TextoHeader=styled.h1`
+font-size:2rem;
+margin:0;
+font-family:'Slabo 27px',serif;
+text-align:center;
+`;
 const Header = ({titulo}) => {
     return ( 
         <ContenedotHeader>
-        <h1>{titulo}</h1>
+        <TextoHeader>{titulo}</TextoHeader>
         </ContenedotHeader>
      );
 }
