0fe98b3f2bc69806cd23cd6246a85256339f113a
Juan Daniel <juanda554242@gmail.com>
2020-10-18 14:39:24 -0500

    instalando y creando los styled conmponents npm i @emotion/styled @emotion/core

--- basicos/cotizador/src/components/Header.js
+++ basicos/cotizador/src/components/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const ContenedotHeader=styled.header`
+    background-color:#d21000;
+    padding:10px;
+    font-weight:bold;
+    color:#FFFFFF;
+`;
 
 const Header = ({titulo}) => {
     return ( 
-        <header>
+        <ContenedotHeader>
         <h1>{titulo}</h1>
-        </header>
+        </ContenedotHeader>
      );
 }
  
