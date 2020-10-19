f02a4363acf3a0a85a1b08c91ee0e8ec2d94814d
Juan Daniel <juanda554242@gmail.com>
2020-10-18 19:38:26 -0500

    documentando el c resultado

--- basicos/cotizador/src/components/Resultado.js
+++ basicos/cotizador/src/components/Resultado.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import PropTypes from 'prop-types';
 import {TransitionGroup,CSSTransition} from 'react-transition-group';
 
 const Mensaje=styled.p`
@@ -48,5 +49,7 @@ const Resultado = ({cotizacion}) => {
         )
      );
 }
- 
+Resultado.propTypes={
+    cotizacion:PropTypes.number.isRequired
+}
 export default Resultado;
