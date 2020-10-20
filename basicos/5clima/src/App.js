d88bf145924b1880b167cc714a8284b905a74b60
Juan Daniel <juanda554242@gmail.com>
2020-10-19 19:40:49 -0500

    agreando el input y el select

--- basicos/5clima/src/App.js
+++ basicos/5clima/src/App.js
@@ -1,4 +1,5 @@
 import React,{Fragment} from 'react';
+import Formulario from './components/Formulario';
 import Header from './components/Header';
 
 
@@ -8,6 +9,18 @@ function App() {
     <Header
       titulo='Clima JDVPL'
     />
+    <div className="contenedor-form">
+      <div className="container">
+        <div className="row">
+          <div className="col m6 s12">
+            <Formulario/>
+          </div>
+          <div className="col m6 s12">
+            2
+          </div>
+        </div>
+      </div>
+    </div>
     </Fragment>
       
 
