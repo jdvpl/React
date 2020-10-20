d44f232781739031184f3cb4c8997d319c6f8511
Juan Daniel <juanda554242@gmail.com>
2020-10-19 23:50:21 -0500

    el hooks personalizado

--- intermedios/1criptomonedas/src/hooks/useMoneda.js
+++ intermedios/1criptomonedas/src/hooks/useMoneda.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React,{Fragment,useState} from 'react';
 
 const useMoneda=()=>{
-    const seleccionar=()=>{
-        
-    }
+    // state de nuestro custom hook
+    const [state,actualizarState]=useState('')
+    const seleccionar=()=>(
+        <Fragment>
+        <Label>{label}</Label>
+        <Select >
+            <option value="">- Seleccione -</option>
+            <option value="COP">Peso Mexicano</option>
+            <option value="MXN">Peso Mexicano</option>
+        </Select>
+    </Fragment>
+    );
+    // retornar state interfaz que modifica el state
+    return[state,seleccionar,actualizarState];
 }
+
 export default useMoneda;
+
+const [moneda,guardarMoneda]=useState();
+
