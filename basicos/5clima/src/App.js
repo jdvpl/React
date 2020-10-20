1193e9ba876c11302b2433d5d4dd6af4fe7eb695
Juan Daniel <juanda554242@gmail.com>
2020-10-19 19:30:01 -0500

    creando el header y dandole estilos con materialize

--- basicos/5clima/src/App.js
+++ basicos/5clima/src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React,{Fragment} from 'react';
+import Header from './components/Header';
 
 
 function App() {
   return (
-      <h1>Clima del mundo</h1>
+    <Fragment>
+    <Header
+      titulo='Clima JDVPL'
+    />
+    </Fragment>
+      
 
   );
 }
