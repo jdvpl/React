367d99a90a81c5b57f3be65ef269effc470bd61a
Juan Daniel <juanda554242@gmail.com>
2020-10-28 22:13:18 -0500

    toques del custom Hook del categorias

--- intermedios/3noticias/src/hooks/useSelect.js
+++ intermedios/3noticias/src/hooks/useSelect.js
@@ -8,6 +8,7 @@ const UseSelect = (stateInicial,opciones) => {
         <select 
             className="browser-default"
             value={state}
+            onChange={e=>actualizarState(e.target.value)}
             >
             {opciones.map(opcion=>(
                 <option key={opcion.value} value={opcion.value}>{opcion.label}</option>
