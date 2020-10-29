09f654efb693dd722dbba161c315b1b68c262710
Juan Daniel <juanda554242@gmail.com>
2020-10-28 20:05:38 -0500

    dandole estilos desde un module tener en cuenta el _ poara nombrar las clases en css de lo contrario si usas - tocar usar el ['....']

--- intermedios/3noticias/src/components/Formulario.js
+++ intermedios/3noticias/src/components/Formulario.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import styles from './Fomulario.module.css'
+import styles from './Fomulario.module.css';
 
 
 const Formulario = () => {
     return ( 
-        <div className="row">
+        <div className={`${styles.buscador} row`}>
             <div className="col s12 m8 offset-m2"> 
                 <form>
-                    <h2>Encuentra noticias por categoria</h2>
+                    <h2 className={styles.heading}>Encuentra noticias por categoria</h2>
                     <div className="input-field col s12">
                         <input
                             type="submit"
-                            className="btn-large amber darken-2"
+                            className={`${styles.['btn-block']} btn-large amber darken-2`}
                             value="buscar"
                         />
 
