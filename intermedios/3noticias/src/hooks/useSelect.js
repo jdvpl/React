ced71936ed7e6c16a98898cdc87d9c27530c332e
Juan Daniel <juanda554242@gmail.com>
2020-10-28 22:08:26 -0500

    colocando el state inicial del sitio web

--- intermedios/3noticias/src/hooks/useSelect.js
+++ intermedios/3noticias/src/hooks/useSelect.js
@@ -5,7 +5,10 @@ import React, { useState } from 'react';
 const UseSelect = (stateInicial,opciones) => {
     const [state,actualizarState]=useState(stateInicial);
     const SelectNoticias=()=>(
-        <select className="browser-default">
+        <select 
+            className="browser-default"
+            value={state}
+            >
             {opciones.map(opcion=>(
                 <option key={opcion.value} value={opcion.value}>{opcion.label}</option>
             ))}
