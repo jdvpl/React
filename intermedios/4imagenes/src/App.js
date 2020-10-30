6b330df1aba763af5cbecf12caee7fbb40059276
Juan Daniel <juanda554242@gmail.com>
2020-10-30 13:11:26 -0500

    creando el jumbotron

--- intermedios/4imagenes/src/App.js
+++ intermedios/4imagenes/src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 function App() {
   return (
-   <h1>Imgenes de pixaby</h1> 
+   <div className="container">
+    <div className="jumbotron">
+      <p className="lead text-center">Buscador de imagenes</p>
+    </div>
+   </div>
   );
 }
 
