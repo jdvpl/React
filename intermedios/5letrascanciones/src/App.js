b76ce76a78bc4eafba1495aea7f12beef8a43efb
Juan Daniel <juanda554242@gmail.com>
2020-10-31 07:54:19 -0500

    uttilizando useefect para que al inicio no regrese nada

--- intermedios/5letrascanciones/src/App.js
+++ intermedios/5letrascanciones/src/App.js
@@ -2,7 +2,9 @@ import React,{Fragment,useEffect, useState} from 'react';
 import Formulario from './components/Formulario';
 function App() {
   const [busquedaletra,guardarBusquedaLetra]=useState({})
-
+  useEffect(()=>{
+    if(Object.keys(busquedaletra).length===0)return;
+  },[busquedaletra])
  
   return (
     <Fragment>
