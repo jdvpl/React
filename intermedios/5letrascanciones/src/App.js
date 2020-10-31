e2831c0505f65a30e8061265c86eebf1da9deb63
Juan Daniel <juanda554242@gmail.com>
2020-10-31 07:28:03 -0500

    estilos del formulario

--- intermedios/5letrascanciones/src/App.js
+++ intermedios/5letrascanciones/src/App.js
@@ -1,7 +1,12 @@
-
+import React,{Fragment,useEffect} from 'react';
+import Formulario from './components/Formulario';
 function App() {
+
+ 
   return (
-    <h1>Letra de canciones</h1>
+    <Fragment>
+      <Formulario/>
+    </Fragment>
   );
 }
 
