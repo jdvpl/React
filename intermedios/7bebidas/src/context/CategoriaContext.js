6afed3c802cd5c483b91bd7188ce5f14b52d524e
Juan Daniel <juanda554242@gmail.com>
2020-10-31 12:28:25 -0500

    creando el provider del context

--- intermedios/7bebidas/src/context/CategoriaContext.js
+++ intermedios/7bebidas/src/context/CategoriaContext.js
@@ -1,4 +1,11 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 
 // crear el context
 export const CategoriasContext=createContext();
+// provider es donde se encuentran las funciones y state
+
+const CategoriasProvider=(props)=>{
+    // crear el state del context
+    const [hola,guardarHola]=useState('hola');
+    
+}
