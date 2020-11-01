8cffa7eecfac3dde647fe84c717aebd2c49d2ecf
Juan Daniel <juanda554242@gmail.com>
2020-11-01 13:05:55 -0500

    terminando el aside

--- avanzados/1MearTask/cliente/src/components/proyectos/Sidebar.js
+++ avanzados/1MearTask/cliente/src/components/proyectos/Sidebar.js
@@ -4,6 +4,9 @@ const Sidebar = () => {
     return ( 
         <aside>
             <h1>MERN<span>Tasks</span></h1>
+            <div className="proyectos">
+                <h2>Tus Proyectos</h2>
+            </div>
         </aside>
     );
 }
