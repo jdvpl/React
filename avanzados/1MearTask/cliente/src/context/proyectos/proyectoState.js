3eac5c315a08840ca0790a73e3b8d0d6d7da0a7f
Juan Daniel <juanda554242@gmail.com>
2020-11-01 16:16:45 -0500

    creadno el provider para el formulario para que no se muestre

--- avanzados/1MearTask/cliente/src/context/proyectos/proyectoState.js
+++ avanzados/1MearTask/cliente/src/context/proyectos/proyectoState.js
@@ -1,11 +1,22 @@
 import React, { useReducer } from 'react';
+import ProyectoContext from './proyectoContext';
 import proyectoReducer from './proyectoReducer';
 
 
 const proyectoState=props=>{
     const initailState={
-        nuevoProyecto:false
+        formulario:false
     }
     // disparch para ejecutar las acciones
     cont [sate,dispatch]=useReducer(proyectoReducer,initailState)
+    // serie de funcioens para el crud
+
+    return(
+        <ProyectoContext.Provider
+        value={{
+            formulario:state.formulario
+        }}>
+            {props.children}
+        </ProyectoContext.Provider>
+    )
 }
