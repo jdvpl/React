9788f597ea2aec794f4c893d7d05b2a603612fbc
Juan Daniel <juanda554242@gmail.com>
2020-11-01 12:07:45 -0500

    colocando las ruta princiapal Login

--- avanzados/1MearTask/cliente/src/App.js
+++ avanzados/1MearTask/cliente/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import {BrowserRouter as router} from 'react-router-dom';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 
 function App() {
   return (
-    <h1>MERNTasks</h1>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login}/>
+      </Switch>
+    </Router>
   );
 }
 
