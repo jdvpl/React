864de700575511a85629ae9e14e7c13f32a13ac0
Juan Daniel <juanda554242@gmail.com>
2020-11-01 13:02:51 -0500

    creandno el componente para el aside

---
+++ avanzados/1MearTask/cliente/src/components/proyectos/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return ( 
+        <aside>
+            <h1>MERN<span>Tasks</span></h1>
+        </aside>
+    );
+}
+ 
+export default Sidebar;
