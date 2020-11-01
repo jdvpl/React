16bc7a27dccf39ab97f09d957246558f93370252
Juan Daniel <juanda554242@gmail.com>
2020-10-31 23:39:54 -0500

    useEffect para llamar la api de la recerta

--- intermedios/7bebidas/src/context/ModalContext.js
+++ intermedios/7bebidas/src/context/ModalContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 
 // crear el context
 export const ModalContext=createContext();
@@ -6,6 +6,11 @@ export const ModalContext=createContext();
 const ModalProvider = (props) => {
     // state del provider VA ASE nullo porque asi debe ser inicial hasta que el usuario presion una que el guste
     const [idreceta,guardarIdReceta]=useState(null);
+    // una vez que ya tenemos la receta llamar la api
+
+    useEffect(()=>{
+
+    },[idreceta])
     return ( 
         <ModalContext.Provider
         value={{
