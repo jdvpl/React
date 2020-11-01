313ee90c7b307493b2dde93ef32462cd6a486dde
Juan Daniel <juanda554242@gmail.com>
2020-10-31 21:35:36 -0500

    creando el provider

--- intermedios/7bebidas/src/context/RecetasContext.js
+++ intermedios/7bebidas/src/context/RecetasContext.js
@@ -1,3 +1,12 @@
 import React, { createContext } from 'react';
 
 export const RecetasContext=createContext();
+const RecetasProvider = (props) => {
+    return (  
+        <RecetasContext.Provider>
+            {props.children}
+        </RecetasContext.Provider>
+    );
+}
+ 
+export default RecetasProvider;
