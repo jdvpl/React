e5dc3cf3a5d94b9fa32289b03f66593cc6e5ffac
Juan Daniel <juanda554242@gmail.com>
2020-11-01 16:23:30 -0500

    pasando el conext para usuar la funcion del formulario en false

--- avanzados/1MearTask/cliente/src/context/proyectos/proyectoState.js
+++ avanzados/1MearTask/cliente/src/context/proyectos/proyectoState.js
@@ -8,13 +8,13 @@ const ProyectoState=props=>{
         formulario:false
     }
     // disparch para ejecutar las acciones
-    cont [sate,dispatch]=useReducer(proyectoReducer,initailState)
+    const [state,dispatch]=useReducer(proyectoReducer,initailState)
     // serie de funcioens para el crud
 
     return(
         <ProyectoContext.Provider
         value={{
-            formulario:state.formulario
+            formulario: state.formulario
         }}>
             {props.children}
         </ProyectoContext.Provider>
