51ff4be72e229d209804ad24ffd46845e46ff28a
Juan Daniel <juanda554242@gmail.com>
2020-11-01 12:20:55 -0500

    creando el label del correro con el input

--- avanzados/1MearTask/cliente/src/components/auth/Login.js
+++ avanzados/1MearTask/cliente/src/components/auth/Login.js
@@ -2,7 +2,24 @@ import React from 'react';
 
 const Login = () => {
     return ( 
-        <h1>Desde Login</h1>
+        <div className="form-usuario">
+        <div className="contenedor-form sombra-dark">
+            <h1>Inciar Sesion</h1>
+            <form>
+                <div className="campo-form">
+                    <label htmlFor="email">Correo</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="Correo"
+                        onChange={onChange}
+                        />
+                </div>
+
+            </form>
+        </div>
+        </div>
      );
 }
  
