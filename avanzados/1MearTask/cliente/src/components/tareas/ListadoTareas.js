7a2a21ed5016403258d7a2fa07f679ed098e5321
Juan Daniel <juanda554242@gmail.com>
2020-11-01 15:19:34 -0500

    terminando diseño del listado de tareas

--- avanzados/1MearTask/cliente/src/components/tareas/ListadoTareas.js
+++ avanzados/1MearTask/cliente/src/components/tareas/ListadoTareas.js
@@ -25,6 +25,10 @@ const ListadoTareas = () => {
 
                 }
             </ul>
+            <button
+                type="button"
+                className="btn btn-eliminar"
+                >Eliminar Proyecto &times;</button>
         </Fragment>
      );
 }
