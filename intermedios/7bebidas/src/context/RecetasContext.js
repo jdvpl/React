8b2b1dc80de391c7a48e7d65032b47364c4ccc1c
Juan Daniel <juanda554242@gmail.com>
2020-10-31 21:58:36 -0500

    useEffect para traer los datos de la api

--- intermedios/7bebidas/src/context/RecetasContext.js
+++ intermedios/7bebidas/src/context/RecetasContext.js
@@ -1,12 +1,21 @@
-import React, { createContext, useState } from 'react';
+import Axios from 'axios';
+import React, { createContext, useEffect, useState } from 'react';
 
 export const RecetasContext=createContext();
 const RecetasProvider = (props) => {
     const [recetas,guardarRecetas]=useState([]);
     const [busqueda,buscarRecetas]=useState({
-        ingrediente:'',
+        nombre:'',
         categoria:''
-    })
+    });
+    useEffect(()=>{
+        const obtenerRecetas=async()=>{
+            const url=`https://www.thecocktaildb.com/api/json/v1/1/search.php?i=vodka&c=Cocoa`;
+            const receta=await Axios(url);
+            console.log(receta);
+        }
+        obtenerRecetas();
+    },[busqueda])
     return (  
         <RecetasContext.Provider
             value={{
