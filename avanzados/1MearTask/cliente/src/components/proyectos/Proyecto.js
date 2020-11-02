bc0535c575180ad04f3bda71d45a229de9efc8fe
Juan Daniel <juanda554242@gmail.com>
2020-11-02 18:06:32 -0500

    pasando la funcion para filtar las tareas

--- avanzados/1MearTask/cliente/src/components/proyectos/Proyecto.js
+++ avanzados/1MearTask/cliente/src/components/proyectos/Proyecto.js
@@ -1,19 +1,28 @@
 import React, { useContext } from 'react';
 import ProyectoContext from '../../context/proyectos/proyectoContext';
+import tareaContext from '../../context/tareas/tareaContext';
+
 
 const Proyecto = ({proyecto}) => {
     // state del proyecto
     const proyectosContext = useContext(ProyectoContext);
     // extrayendo la funcionalidad del formulario
     const {proyectoActual}=proyectosContext;
-
+    // obtener la funcion del context de tarea
+    const tareasContext=useContext(tareaContext);
+    const {ObtenerTareas}=tareasContext;
+    // funcion para agregar el proyecto actual
+    const SeleccionarProyecto=id=>{
+        proyectoActual(id); //fijar un proyecto actual
+        ObtenerTareas(id); //filtar las tareas cuando se de clicik
+    }
 
     return ( 
         <li>
             <button
                 type="button"
                 className="btn btn-blank"
-                onClick={()=>proyectoActual(proyecto.id)}
+                onClick={()=>SeleccionarProyecto(proyecto.id)}
             >
                 {proyecto.nombre}
             </button>
