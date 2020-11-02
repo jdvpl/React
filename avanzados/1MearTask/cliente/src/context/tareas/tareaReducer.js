457af09506659218d47bee99dd3a463bd7564489
Juan Daniel <juanda554242@gmail.com>
2020-11-02 18:33:51 -0500

    utilizando el reducer para cuando haga se funcion se ejecute

--- avanzados/1MearTask/cliente/src/context/tareas/tareaReducer.js
+++ avanzados/1MearTask/cliente/src/context/tareas/tareaReducer.js
@@ -1,4 +1,4 @@
-import { TAREAS_PROYECTO } from '../../types';
+import { TAREAS_PROYECTO,AGREGAR_TAREA } from '../../types';
 
 export default (state,action)=>{
     switch(action.type){
@@ -6,6 +6,11 @@ export default (state,action)=>{
         return{
             ...state,
             tareasproyecto:state.tareas.filter(tarea=>tarea.proyectoId===action.payload)
+        }
+        case AGREGAR_TAREA:
+        return{
+            ...state,
+            tareas:[...state.tareas,action.payload]
         }
         default:
         return state;
