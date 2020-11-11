799dec10b99729cc44b8a2f3f3325eeef229b6b2
Juan Daniel <juanda554242@gmail.com>
2020-11-10 21:14:16 -0500

    definiendo la ruta de la pagina princiapal desde el index

--- avanzados/1MearTask/servidor/index.js
+++ avanzados/1MearTask/servidor/index.js
@@ -7,6 +7,11 @@ const app=express();
 // se crea el puerto
 const PORT =process.env.PORT || 4000;
 
+// definir la pagina principal
+app.get('/',(req,res)=>{
+    res.send(`<h1>Insectos depravados</h1>`);
+});
+
 // arrancar la app
 app.listen(PORT,()=>{
     console.log(`corriendo desde el puerto ${PORT}`)
