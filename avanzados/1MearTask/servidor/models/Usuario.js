36c10446ac5fc3d3f6adf77f18917f2a02cc1664
Juan Daniel <juanda554242@gmail.com>
2020-11-11 22:10:02 -0500

    definiendo el modelo de la base de datos

--- avanzados/1MearTask/servidor/models/Usuario.js
+++ avanzados/1MearTask/servidor/models/Usuario.js
@@ -1,6 +1,51 @@
 const mongoose=require('mongoose');
 
 const usuariosSchema=mongoose.Schema({
+    nombre:{
+        type:String,
+        require:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        require:true,
+        trim:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        require:true,
+        trim:true
+    
+    },
+    registro:{
+        type:Date,
+        default:Date.now()
+    },
+    linkFacebook:{
+        type:String,
+        trim:true
+
+    },
+    linkGithub:{
+        type:String,
+        trim:true
+    },
+    telefono:{
+        type:String,
+        trim:true
+
+    },
+    sitioWeb:{
+        type:String,
+        trim:true
+    },
+    foto:{
+        type:String,
+        trim:true
+
+    }
+
 
 });
 
