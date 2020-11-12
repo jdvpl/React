f84fa22e6b0dbb5ac9e842001a00919b909d1745
Juan Daniel <juanda554242@gmail.com>
2020-11-11 21:43:29 -0500

    importando la funciones el usuario

--- avanzados/1MearTask/servidor/routes/usuarios.js
+++ avanzados/1MearTask/servidor/routes/usuarios.js
@@ -1,6 +1,7 @@
 // rutas para crear usuariose
 const express=require('express');
 const router=express.Router();
+const usuarioController=require('../controllers/usuarioController');
 
 // crea un usuario
 
