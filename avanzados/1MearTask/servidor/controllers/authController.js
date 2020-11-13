1eaa433f475b1c44f8ae021611495965304b5b1e
Juan Daniel <juanda554242@gmail.com>
2020-11-13 00:15:39 -0500

    pasando el funcionaminto del controller a la roter

--- avanzados/1MearTask/servidor/controllers/authController.js
+++ avanzados/1MearTask/servidor/controllers/authController.js
@@ -4,5 +4,10 @@ const {validationResult}=require('express-validator');
 const jwt =require('jsonwebtoken');
 
 exports.autenticarUsuario=async(req,res)=>{
+    // valdiar si hay errores
+    const errores =validationResult(req);
+    if(!errores.isEmpty()){
+        return res.status(400).json({errores:errores.array()});
+    }
 
 }
--- avanzados/1MearTask/servidor/routes/auth.js
+++ avanzados/1MearTask/servidor/routes/auth.js
@@ -1,7 +1,7 @@
 // rutas para autenticar usuarios
 const express=require('express');
 const router=express.Router();
-
+const authController=require('../controllers/authController');
 // dependemncia para validar 
 const {check} =require('express-validator')
 
@@ -13,5 +13,5 @@ router.post('/',
         check('email','El correo es obligatorio').isEmail(),
         check('password','La contraseña debe ser minimo de 6 caracteres').isLength({min:6})
     ],
-    );
+    authController.autenticarUsuario);
 module.exports=router;
