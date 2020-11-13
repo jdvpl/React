6a6b78a86dd528dbecefe4723d9d53f0a37e31a5
Juan Daniel <juanda554242@gmail.com>
2020-11-13 00:10:43 -0500

    creando el controller de autenticacion del usuario

---
+++ avanzados/1MearTask/servidor/controllers/authController.js
@@ -0,0 +1,8 @@
+const Usuario=require('../models/Usuario');
+const bcrypts=require('bcryptjs');
+const {validationResult}=require('express-validator');
+const jwt =require('jsonwebtoken');
+
+exports.autenticarUsuario=async(req,res)=>{
+
+}
--- avanzados/1MearTask/servidor/routes/auth.js
+++ avanzados/1MearTask/servidor/routes/auth.js
@@ -10,8 +10,6 @@ const {check} =require('express-validator')
 // endpoint /api/usuarios
 router.post('/',
     [
-        check('nombre','El nombre es obligatorio').not().isEmpty(),
-        check('username','El usuario es obligatorio').not().isEmpty(),
         check('email','El correo es obligatorio').isEmail(),
         check('password','La contraseña debe ser minimo de 6 caracteres').isLength({min:6})
     ],
