88c4764e62d0fb933b2803fe6f55a5b98c7358a3
Juan Daniel <juanda554242@gmail.com>
2020-11-14 00:10:19 -0500

    agregando nuevo proyecto

--- avanzados/1MearTask/servidor/controllers/proyectoController.js
+++ avanzados/1MearTask/servidor/controllers/proyectoController.js
@@ -1,4 +1,12 @@
+const Proyecto=require('../models/Poryecto');
 
 exports.crearProyecto=async(req,res)=>{
-
+    try {
+        const proyecto=new Proyecto(req.body);
+        proyecto.save();
+        res.json(proyecto);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
 }
