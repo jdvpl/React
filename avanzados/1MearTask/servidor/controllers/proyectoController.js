65a0482c6907a1c68015a233ed3cdd333c617a66
Juan Daniel <juanda554242@gmail.com>
2020-11-14 11:48:16 -0500

    eliminacion de un proyecto

--- avanzados/1MearTask/servidor/controllers/proyectoController.js
+++ avanzados/1MearTask/servidor/controllers/proyectoController.js
@@ -72,5 +72,22 @@ exports.ActualizarProyecto=async(req,res)=>{
 }
 
 exports.EliminarProyecto=async(req,res)=>{
-    
+    try {
+         // revisar el id
+         let proyecto=await Proyecto.findById(req.params.id);
+         // si el proyecto existe o no
+         if(!proyecto){
+             return res.status(404).json({msg:'Proyecto no encontrado'});
+         }
+         // verificar el creador
+         if(proyecto.creador.toString() !==req.usuario.id){
+             return res.status(401).json({msg:'No Autorizado'});
+         }
+         //Eliminar el proyecto
+         await Proyecto.findOneAndRemove({_id:req.params.id});
+         res.json({msg:'Proyecto eliminado'});
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Error en el servidor');
+    }
 }
