7d65c96cf1b7f93527ab851d52bde6ba279ba4f7
Juan Daniel <juanda554242@gmail.com>
2020-11-14 17:55:39 -0500

    definiendo el state principal de la autenticacion

--- avanzados/1MearTask/cliente/src/context/autenticacion/authReducer.js
+++ avanzados/1MearTask/cliente/src/context/autenticacion/authReducer.js
@@ -1,17 +1,16 @@
-import { 
-    REGISTRO_EXISTOSO,
-    REGISTRO_ERROR,
-    OBTENER_USUARIO,
-    LOGIN_EXITOSO,
-    LOGIN_ERROR,
-    CERRAR_SESION
-} from '../../types/index';
+import {
+  REGISTRO_EXISTOSO,
+  REGISTRO_ERROR,
+  OBTENER_USUARIO,
+  LOGIN_EXITOSO,
+  LOGIN_ERROR,
+  CERRAR_SESION,
+} from "../../types/index";
 
-const authReducer= (state,action)=>{
-    switch(action.type){
-        
-        default:
-            return state;
-    }
-}
+const authReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
 export default authReducer;
---
+++ avanzados/1MearTask/cliente/src/context/autenticacion/authState.js
@@ -0,0 +1,15 @@
+import React,{useReducer} from 'react';
+import {
+    REGISTRO_EXISTOSO,
+    REGISTRO_ERROR,
+    OBTENER_USUARIO,
+    LOGIN_EXITOSO,
+    LOGIN_ERROR,
+    CERRAR_SESION,
+  } from '../../types';
+import AuthContext from './authContext';
+import AuthReducer from './authReducer';
+const AuthState=props=>{
+
+}
+export default AuthState;
