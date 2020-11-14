2366d5f3fb53f6ab4326dc5d00470cf7a5ade136
Juan Daniel <juanda554242@gmail.com>
2020-11-14 00:29:28 -0500

    creando el middleware para la validacion de ltoken en general

---
+++ avanzados/1MearTask/servidor/middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt=require('jsonwebtoken');
+
+module.exports=function(req,res,next){
+    // leer el token del header
+
+
+
+    // revidar si no hay token
+
+
+
+    // validar el token
+
+
+
+
+    
+}
