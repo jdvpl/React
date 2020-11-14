7c36be0146ebce255e27caa1d378773a84af6c13
Juan Daniel <juanda554242@gmail.com>
2020-11-14 00:31:06 -0500

    leer el token del header

--- avanzados/1MearTask/servidor/middleware/auth.js
+++ avanzados/1MearTask/servidor/middleware/auth.js
@@ -2,7 +2,8 @@ const jwt=require('jsonwebtoken');
 
 module.exports=function(req,res,next){
     // leer el token del header
-
+    const token=req.header('x-auth-token');
+    console.log(token)
 
 
     // revidar si no hay token
@@ -14,5 +15,5 @@ module.exports=function(req,res,next){
 
 
 
-    
+
 }
