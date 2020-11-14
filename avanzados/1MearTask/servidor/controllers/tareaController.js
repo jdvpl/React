37cf7b94f1cb453dc6184e2bbec06e25c50d7a65
Juan Daniel <juanda554242@gmail.com>
2020-11-14 12:25:50 -0500

    creando tareas por proyecto

--- avanzados/1MearTask/servidor/controllers/tareaController.js
+++ avanzados/1MearTask/servidor/controllers/tareaController.js
@@ -9,16 +9,24 @@ exports.crearTarea=async(req,res)=>{
     if(!errores.isEmpty()){
         return res.status(400).json({errores:errores.array()});
     }
-    // extraer el proyecto y comprobar si existe
-    const {proyecto}=req.body;
+   
     try {
-        const proyecto=await Proyecto.findById(proyecto);
-        if(!proyecto){
+         // extraer el proyecto y comprobar si existe
+        const {proyecto}=req.body;
+        const existeProyecto=await Proyecto.findById(proyecto);
+        if(!existeProyecto){
             return res.status(404).json({msg:'Poryecto no encontrado'});
 
         }
         // revisar si el proyecto actual pertenece al usuario autenticado
-        
+        if(existeProyecto.creador.toString() !==req.usuario.id){
+            return res.status(401).json({msg:'No Autorizado'});
+        }
+        // creamos la tarea
+        const tarea=new Tarea(req.body);
+        await tarea.save();
+        res.json({tarea})
+
     } catch (error) {
         console.log(error);
         res.status(500).send('Hubo un error');
