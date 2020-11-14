ee46eec037f68b78d456b0f2442555474376b746
Juan Daniel <juanda554242@gmail.com>
2020-11-14 18:04:23 -0500

    pasando el authState al componente principal

--- avanzados/1MearTask/cliente/src/App.js
+++ avanzados/1MearTask/cliente/src/App.js
@@ -6,12 +6,14 @@ import Proyectos from './components/proyectos/Proyectos';
 import ProyectoState from './context/proyectos/proyectoState';
 import TareaState from './context/tareas/tareaState';
 import AlertaState from './context/alertas/alertaState';
+import AuthState from './context/autenticacion/authState';
 
 function App() {
   return (
     <ProyectoState>
     <TareaState>
     <AlertaState>
+    <AuthState>
       <Router>
       <Switch>
         <Route exact path="/" component={Login}/>
@@ -19,6 +21,7 @@ function App() {
         <Route exact path="/proyectos" component={Proyectos}/>
       </Switch>
     </Router>
+    </AuthState>
     </AlertaState>
     </TareaState>
     </ProyectoState>
