b8545a9d8f25d133ab48156c05b088cd595b8580
Juan Daniel <juanda554242@gmail.com>
2020-11-14 15:03:15 -0500

    la sintaxis del state de alertas

--- avanzados/1MearTask/cliente/src/context/alertas/alertaState.js
+++ avanzados/1MearTask/cliente/src/context/alertas/alertaState.js
@@ -2,3 +2,18 @@ import React,{useReducer} from 'react';
 import alertaReducer from './alertaReducer';
 import alertaContext from './alertaContext';
 import { MOSTRAR_ALERTA,OCULTAR_ALERTA } from '../../types/index';
+
+const AlertaState=props=>{
+    const initailState={
+        alerta:null
+    }
+    
+    return(
+        <alertaContext.Provider
+
+        >
+            {props.children}
+        </alertaContext.Provider>
+    )
+}
+export default AlertaState;
