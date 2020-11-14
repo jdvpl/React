f5a8f4face98b7cf8c1b3c211c8dec4521e9c66b
Juan Daniel <juanda554242@gmail.com>
2020-11-14 09:24:47 -0500

    colocando en el navedador el get para ver si hay un token

--- avanzados/1MearTask/servidor/middleware/auth.js
+++ avanzados/1MearTask/servidor/middleware/auth.js
@@ -3,11 +3,12 @@ const jwt=require('jsonwebtoken');
 module.exports=function(req,res,next){
     // leer el token del header
     const token=req.header('x-auth-token');
-    console.log(token)
-
+    // res.json({token})
 
     // revidar si no hay token
-
+    if(!token){
+        return res.status(401).json({msg:'No hay Token, permiso denegado'})
+    }
 
 
     // validar el token
--- avanzados/1MearTask/servidor/routes/proyectos.js
+++ avanzados/1MearTask/servidor/routes/proyectos.js
@@ -1,11 +1,14 @@
 const express=require('express');
 const router =express.Router();
 const proyectoController=require('../controllers/proyectoController');
-const auth=require('./middleware/auth');
+const auth=require('../middleware/auth');
 // crea proyectos 
 // api/proyectos
 router.post('/',
     auth,
     proyectoController.crearProyecto);
 
+router.get('/',
+auth,
+proyectoController.crearProyecto);
 module.exports=router;
