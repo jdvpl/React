f526715980bc121a05d9f3111b0170538abe0e4f
Juan Daniel <juanda554242@gmail.com>
2020-11-14 12:15:59 -0500

    extrayendo el proyecto

--- avanzados/1MearTask/servidor/controllers/tareaController.js
+++ avanzados/1MearTask/servidor/controllers/tareaController.js
@@ -9,6 +9,20 @@ exports.crearTarea=async(req,res)=>{
     if(!errores.isEmpty()){
         return res.status(400).json({errores:errores.array()});
     }
-    
+    // extraer el proyecto y comprobar si existe
+    const {proyecto}=req.body;
+    try {
+        const proyecto=await Proyecto.findById(proyecto);
+        if(!proyecto){
+            return res.status(404).json({msg:'Poryecto no encontrado'});
+
+        }
+        // revisar si el proyecto actual pertenece al usuario autenticado
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
+
 
 }
