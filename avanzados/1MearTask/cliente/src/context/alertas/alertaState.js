a72aa5d8fbd94428c37f64701abb0f2f461b9515
Juan Daniel <juanda554242@gmail.com>
2020-11-14 15:09:20 -0500

    terminando la funione de mostar la alerta

--- avanzados/1MearTask/cliente/src/context/alertas/alertaState.js
+++ avanzados/1MearTask/cliente/src/context/alertas/alertaState.js
@@ -7,10 +7,27 @@ const AlertaState=props=>{
     const initailState={
         alerta:null
     }
-    
+    const [state,dispatch]=useReducer(alertaReducer,initailState);
+    const MostrarAlerta=(msg,categoria)=>{
+        dispatch({
+            type:MOSTRAR_ALERTA,
+            payload:{
+                msg,
+                categoria
+            }
+        });
+        setTimeout(() => {
+            dispatch({
+                type:OCULTAR_ALERTA
+            })
+        }, 5000);
+    }
     return(
         <alertaContext.Provider
-
+            value={{
+                alerta:state.alerta,
+                MostrarAlerta
+            }}
         >
             {props.children}
         </alertaContext.Provider>
