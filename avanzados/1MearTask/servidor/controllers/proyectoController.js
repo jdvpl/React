ccccfa92017ba472565f85aa00598fd42714ec35
Juan Daniel <juanda554242@gmail.com>
2020-11-14 09:55:40 -0500

    creando un proyecto con el id del creador

--- avanzados/1MearTask/servidor/controllers/proyectoController.js
+++ avanzados/1MearTask/servidor/controllers/proyectoController.js
@@ -1,8 +1,14 @@
+const e = require('express');
 const Proyecto=require('../models/Poryecto');
 
 exports.crearProyecto=async(req,res)=>{
     try {
         const proyecto=new Proyecto(req.body);
+
+        // gaurdar el creador via jwt
+        proyecto.creador=req.usuario.id;
+
+        // guardamos el proyectof
         proyecto.save();
         res.json(proyecto);
     } catch (error) {
