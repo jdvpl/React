173d4fa623c42c77176df5cc81a5505da3647099
Juan Daniel <juanda554242@gmail.com>
2020-11-14 17:45:34 -0500

    authReducer

---
+++ avanzados/1MearTask/cliente/src/context/autenticacion/authReducer.js
@@ -0,0 +1,10 @@
+import { } from '../../types/index';
+
+const authReducer= (state,action)=>{
+    switch(action.type){
+        
+        default:
+            return state;
+    }
+}
+export default authReducer;
