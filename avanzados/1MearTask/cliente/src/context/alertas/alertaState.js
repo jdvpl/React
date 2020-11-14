39fd4a588427d10e65eb18c0e3eb6c0d858775c6
Juan Daniel <juanda554242@gmail.com>
2020-11-14 14:59:04 -0500

    asi debe ser la forma de colocar el reducer

---
+++ avanzados/1MearTask/cliente/src/context/alertas/alertaState.js
@@ -0,0 +1,4 @@
+import React,{useReducer} from 'react';
+import alertaReducer from './alertaReducer';
+import alertaContext from './alertaContext';
+import { MOSTRAR_ALERTA,OCULTAR_ALERTA } from '../../types/index';
