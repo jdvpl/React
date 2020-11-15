a435ee6b1a8ca8d92b898563f27c66e78c80c7f6
Juan Daniel <juanda554242@gmail.com>
2020-11-15 10:38:48 -0500

    terminando la pagian de perfil

--- avanzados/1MearTask/cliente/src/components/layout/Barra.js
+++ avanzados/1MearTask/cliente/src/components/layout/Barra.js
@@ -1,6 +1,9 @@
 import React,{useContext,useEffect} from 'react';
+import { Link } from 'react-router-dom';
 import AuthContext from '../../context/autenticacion/authContext';
-
+import imagen from '../../cerrar.png';
+import usuario1 from '../../usuario.png';
+import proyectos from '../../presentacion.png';
 const Barra = () => {
   // extraer la info de la autenticacion
   const authContext=useContext(AuthContext);
@@ -15,7 +18,14 @@ const Barra = () => {
         <div className="app-header">
           {usuario? <p className="nombre-usuario">Hola <span>{usuario.nombre}</span></p> :null}
           <nav className="nav-principal">
-            <a href="#!">Cerrar Sesion</a>
+          <a href="#!" className="img"><img src={imagen} width="20"></img></a>
+          <Link to={'/proyectos'} >
+          <img src={proyectos} width="20"></img>
+            </Link>
+            <Link to={'/perfil'}>
+              <img src={usuario1} width="20"></img>
+            </Link>
+          
           </nav>
         </div>
       );
