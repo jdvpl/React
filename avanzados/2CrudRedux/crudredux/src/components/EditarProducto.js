d282d1efd99d70ad965f3c703d0e518119c46dd6
Juan Daniel <juanda554242@gmail.com>
2020-11-15 22:23:30 -0500

    creando la interfaz de editar producto

--- avanzados/2CrudRedux/crudredux/src/components/EditarProducto.js
+++ avanzados/2CrudRedux/crudredux/src/components/EditarProducto.js
@@ -2,7 +2,41 @@ import React from 'react';
 
 const EditarProducto = () => {
     return ( 
-        <h2>Editando </h2>
+        <div className="row justify-content-center">
+            <div className="col-md-8">
+                <div className="card">
+                    <div className="card-body">
+                        <h2 className="text-center mb-4 font-wight-bold">Editar Producto</h2>
+
+                        <form>
+                            <div className="form-group">
+                                <label>Nombre Producto</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Nombre Producto"
+                                    name="nombre"
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label>Precio</label>
+                                <input
+                                    type="number"
+                                    className="form-control"
+                                    placeholder="Precio"
+                                    name="precio"
+                                />
+                            </div>
+                            <button
+                                type="submit"
+                                className="btn btn-primary font-weight-bold text-capitaliza d-block w-100">
+                                    Guardar
+                                </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
      );
 }
  
