a023c8a9b8337d4955ea5b7f31b0d0425dd25025
Juan Daniel <juanda554242@gmail.com>
2020-11-15 20:03:04 -0500

    importando reactrouter dom para colocar las rutas

--- avanzados/2CrudRedux/crudredux/src/App.js
+++ avanzados/2CrudRedux/crudredux/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import Header from './components/Header';
 
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 function App() {
   return (
-      <Header/>
+    <Router>
+            <Header/>
+            <div>
+              <Switch>
+                <Route exact path="/" component={}/>
+              </Switch>
+            </div>
+    </Router>
+
   );
 }
 
--- avanzados/2CrudRedux/crudredux/src/components/Header.js
+++ avanzados/2CrudRedux/crudredux/src/components/Header.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 
+
+
 const Header = () => {
     return (  
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary justify-content-between">
