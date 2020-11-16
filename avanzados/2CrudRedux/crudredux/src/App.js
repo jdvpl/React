1c2bd54b10abd8d4c86244b213fd5182b6a64b78
Juan Daniel <juanda554242@gmail.com>
2020-11-15 20:21:00 -0500

    creando los componentes para cambiar y colocar Link es mas rapido

--- avanzados/2CrudRedux/crudredux/src/App.js
+++ avanzados/2CrudRedux/crudredux/src/App.js
@@ -4,14 +4,17 @@ import Header from './components/Header';
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 import Productos from './components/Productos';
 import NuevoProducto from './components/NuevoProducto';
+import EditarProducto from './components/EditarProducto';
 function App() {
   return (
     <Router>
             <Header/>
-            <div className="container">
+            <div className="container mt-5">
               <Switch>
                 <Route exact path="/" component={Productos}/>
                 <Route exact path="/productos/nuevo" component={NuevoProducto}/>
+                <Route exact path="/productos/editar/:id" component={EditarProducto}/>
+             
               </Switch>
             </div>
     </Router>
---
+++ avanzados/2CrudRedux/crudredux/src/components/EditarProducto.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const EditarProducto = () => {
+    return ( 
+        <h2>Editando </h2>
+     );
+}
+ 
+export default EditarProducto;
