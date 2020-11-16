d1ab2a9b09f018ad78483bc2c94de4da6e6d96a8
Juan Daniel <juanda554242@gmail.com>
2020-11-15 22:46:41 -0500

    definiendo el reducer]

---
+++ avanzados/2CrudRedux/crudredux/src/reducers/proudctosReducers.js
@@ -0,0 +1,15 @@
+// cada reduceres tiene su porpio estate
+
+const initialState={
+    productos:[],
+    error:null,
+    loading:false
+}
+
+export default function(state=initialState,action){
+    switch(action.type){
+        
+        default:
+            return state;
+    }
+}
