1421295029ee393da433416af893c0a372ed976a
Juan Daniel <juanda554242@gmail.com>
2020-11-15 20:09:11 -0500

    componente de nuevo producto

--- avanzados/2CrudRedux/crudredux/src/App.js
+++ avanzados/2CrudRedux/crudredux/src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Header from './components/Header';
 
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import Productos from './components/Productos';
+import NuevoProducto from './components/NuevoProducto';
 function App() {
   return (
     <Router>
             <Header/>
-            <div>
+            <div className="container">
               <Switch>
-                <Route exact path="/" component={}/>
+                <Route exact path="/" component={Productos}/>
+                <Route exact path="/productos/nuevo" component={NuevoProducto}/>
               </Switch>
             </div>
     </Router>
---
+++ avanzados/2CrudRedux/crudredux/src/components/NuevoProducto.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NuevoProducto = () => {
+    return ( 
+        <form className="form-group">
+            <input
+                type="text"
+            />
+        </form>
+     );
+}
+ 
+export default NuevoProducto;
