d9e1ba7ded66082459d8e38f957e18c938ae3a33
Juan Daniel <juanda554242@gmail.com>
2020-11-15 22:00:19 -0500

    creando formualrio para neuvo proudcto

--- avanzados/2CrudRedux/crudredux/src/components/NuevoProducto.js
+++ avanzados/2CrudRedux/crudredux/src/components/NuevoProducto.js
@@ -2,12 +2,42 @@ import React from 'react';
 
 const NuevoProducto = () => {
     return ( 
-        <form className="form-group">
-            <input
-                type="text"
-            />
-        </form>
-     );
+        <div className="row justify-content-center">
+            <div className="col-md-8">
+                <div className="card">
+                    <div className="card-body">
+                        <h2 className="text-center mb-4 font-wight-bold">Agregar Producto</h2>
+
+                        <form>
+                            <div className="form-group">
+                                <label>Nombre Producto</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Nombre Producto"
+                                    name="nombre"
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label>Precio</label>
+                                <input
+                                    type="number"
+                                    className="form-control"
+                                    placeholder="Precio"
+                                    name="precio"
+                                />
+                            </div>
+                            <button
+                                type="submit"
+                                className="btn btn-primary font-weight-bold text-capitaliza d-block w-100">
+                                    Agregar
+                                </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
  
 export default NuevoProducto;
