5a5a4fe3c715459f0b24b8b6609eb9274580adfa
Juan Daniel <juanda554242@gmail.com>
2020-11-20 00:27:48 -0500

    ceando el sate y pasandole el objeto al porducto

--- avanzados/2CrudRedux/crudredux/src/components/NuevoProducto.js
+++ avanzados/2CrudRedux/crudredux/src/components/NuevoProducto.js
@@ -1,25 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 // funciones de redux
 import {useDispatch,useSelector} from 'react-redux';
 import {crearNuevoProductoAction} from '../actions/productoActions';
 
 const NuevoProducto = () => {
+    const [nombre,setNombre]=useState('');
+    const [precio, setPrecio] = useState(0);
     // utilizar usedispatj y te crea una funcion
     const dispatch=useDispatch();
 
     // mandar a llmar la funcion del action para inplemnetarla
-    const agregarProducto=()=>dispatch(crearNuevoProductoAction())
+    const agregarProducto=producto=>dispatch(crearNuevoProductoAction(producto))
 
     // cuando el usuario haga submit en el boton
     const agregarNUevoProducto=e=>{
         e.preventDefault();
         // validar formulario
-
+        if(nombre.trim()===''||precio<=0){
+            return;
+        }
 
         // sino hay errores
 
         // crear el nuievo productos
-        agregarProducto();
+        agregarProducto({
+            nombre,
+            precio
+        });
     }
     return ( 
         <div className="row justify-content-center">
@@ -38,6 +45,8 @@ const NuevoProducto = () => {
                                     className="form-control"
                                     placeholder="Nombre Producto"
                                     name="nombre"
+                                    value={nombre}
+                                    onChange={e=>setNombre(e.target.value)}
                                 />
                             </div>
                             <div className="form-group">
@@ -47,6 +56,8 @@ const NuevoProducto = () => {
                                     className="form-control"
                                     placeholder="Precio"
                                     name="precio"
+                                    value={precio}
+                                    onChange={e=>setPrecio(Number(e.target.value))}
                                 />
                             </div>
                             <button
