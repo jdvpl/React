d2ad6b35a9715d48bf16923aaddb5e3be14b9408
jdvpl <juanda554242@gmail.com>
2021-05-01 18:21:35 -0500

    utilizar useffect para traer el id solo una vez y userouter para la informacion del id\

--- avanzados/3produchunt/product-hunt-next/pages/productos/[id].js
+++ avanzados/3produchunt/product-hunt-next/pages/productos/[id].js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+
 const Producto = () => {
-    return ( 
-        <div>
-            <h1>desde [id].js</h1>
-        </div>
-     );
-}
- 
+  // roter para obtener el id actual
+  const router = useRouter();
+  const {
+    query: { id },
+  } = router;
+
+  useEffect(() => {
+    if (id) {
+      console.log("ya hay un id");
+    }
+  }, [id]);
+  return (
+    <div>
+      <h1>{id}</h1>
+    </div>
+  );
+};
+
 export default Producto;
